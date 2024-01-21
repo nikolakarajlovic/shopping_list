3df7c245b492cbc7d198b19c513da9a056d01aaf
nikolakarajlovic <95770099+nikolakarajlovic@users.noreply.github.com>
2024-01-21 19:28:02 +0100

    105

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -21,6 +22,7 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      http
       Navigator.of(context).pop(
         GroceryItem(
             id: DateTime.now().toString(),
