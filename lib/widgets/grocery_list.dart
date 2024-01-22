b035018052755aa9208dd9c7c78f920f24a05912
nikolakarajlovic <95770099+nikolakarajlovic@users.noreply.github.com>
2024-01-22 20:03:20 +0100

    108

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/models/category.dart';
 
 // import 'package:shopping_list/data/dummy_items.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
+
+import '../data/categories.dart';
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -12,19 +18,47 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
 
-  void _addItem() async {
-    final newItem = await Navigator.of(context).push<GroceryItem>(
-      MaterialPageRoute(builder: (ctx) => const NewItem()),
-    );
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
 
-    if (newItem == null) {
-      return;
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-393fe-default-rtdb.firebaseio.com', 'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> _loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (catItem) => catItem.value.title == item.value['category'])
+          .value;
+      _loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
     }
     setState(() {
-      _groceryItems.add(newItem);
+      _groceryItems = _loadedItems;
     });
+  }
+
+  void _addItem() async {
+    await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+
+    _loadItems();
   }
 
   void _removeItem(GroceryItem item) {
